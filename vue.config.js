534ff376a700bac9ebbeacac5fbbcfb1a4a914ea
w候人兮猗 <ah_wgs@126.com>
2019-11-12 17:59:25 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -169,7 +169,7 @@ module.exports = {
             priority: 10,
             chunks: "initial" // only package third parties that are initially dependent
           },
-          elementUI: {
+          vantUI: {
             name: "chunk-vantUI", // split vantUI into a single package
             priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
             test: /[\\/]node_modules[\\/]_?vant(.*)/ // in order to adapt to cnpm
